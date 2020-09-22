db1cc9d3fee6a1cd18e620b474919487ae2af752
KeepCrzay <842473809@qq.com>
2020-09-22 11:20:50 +0800

    修改user

---
+++ comss/src/main/java/pojo/test.java
@@ -0,0 +1,16 @@
+package pojo;
+
+/**
+ * 描述:
+ * s
+ *
+ * @ClassName: test
+ * @author:zq
+ * @create 2020-09-22 11:20
+ * @Version:2.1
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
