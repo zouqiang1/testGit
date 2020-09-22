18bfc430350e1d8c053ab31478072c677109e2d6
KeepCrzay <842473809@qq.com>
2020-09-22 11:24:21 +0800

    修改user,添加age属性

--- comss/src/main/java/pojo/User.java
+++ comss/src/main/java/pojo/User.java
@@ -14,6 +14,7 @@ import java.io.Serializable;
 public class User implements Serializable {
     private String name;
     private String email;
+    private Integer age;
 
     public String getName() {
         return name;
